286a2273e1473366f9988bfd6bd6bbfea745df4a
Wysiwyg76 <96958869+Wysiwyg76@users.noreply.github.com>
2026-01-04 21:14:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -205,8 +205,8 @@ function assetSingleMessage(asset, w, m, price, symbol) {
   return (
     `*${asset.name}*\n` +
     `• Prix : *${safe(price)} ${currency}*\n` +
-    `• Hebdomadaire : *${rsiLabel(safe(w?.current))}* (${safe(m?.current)})\n` +
-    `• Mensuel : *${rsiLabel(safe(w?.current))}* (${safe(m?.current)})`
+    `• Hebdomadaire : *${rsiLabel(w?.current)}* (${safe(w?.current)})\n` +
+    `• Mensuel : *${rsiLabel(m?.current)}* (${safe(m?.current)})`
   );
 }
 
